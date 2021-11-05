c097d062ac9bcb7c82a88785a02e46bdd924f96e
Esen Yalım Karaduman <esen.yalim@gmail.com>
2021-11-05 13:06:12 +0300

    create PgAutoCAD project

--- PgAutoCAD/Commands.cs
+++ PgAutoCAD/Commands.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Autodesk.AutoCAD.Runtime;
+
+[assembly: CommandClass(typeof(PgAutoCAD.Commands))]
+
 namespace PgAutoCAD
 {
-    public class Class1
+    public class Commands
     {
+
     }
 }
---
+++ PgAutoCAD/Plugin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Autodesk.AutoCAD.Runtime;
+
+[assembly: ExtensionApplication(typeof(PgAutoCAD.Plugin))]
+
+namespace PgAutoCAD
+{
+    public class Plugin: IExtensionApplication
+    {
+        public void Initialize()
+        {
+            
+        }
+
+        public void Terminate()
+        {
+            
+        }
+    }
+}
